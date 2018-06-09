999193843f8a20364aa13b50e290271b9a9e6592
Krystian <heliox8888@yahoo.pl>
2018-06-09 22:57:21 +0200

    Dodano JpaFactory, model DAO customera i Generic

--- src/jpaDAO/GenericJpaDao.java
+++ src/jpaDAO/GenericJpaDao.java
@@ -4,11 +4,47 @@
  * and open the template in the editor.
  */
 package jpaDAO;
-
+import Interface.GenericDao;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import javax.persistence.EntityManager;
+import factory.JpaFactory;
 /**
  *
  * @author Rozkurwozaur
  */
-public class GenericJpaDao {
-    
+public class GenericJpaDao<T, K> implements GenericDao<T, K> {
+
+    private final Class<T> type;
+
+    public GenericJpaDao() {
+        Type t = getClass().getGenericSuperclass();
+        ParameterizedType pt = (ParameterizedType) t;
+        type = (Class) pt.getActualTypeArguments()[0];
+    }
+    @Override
+    public void save(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T findById(K id) {
+        EntityManager em = getEntityManager();
+        T dto = em.find(type, id);
+        em.close();
+        return dto;
+    }
+    protected EntityManager getEntityManager() {
+        return JpaFactory.getEntityManager();
+    }
 }
